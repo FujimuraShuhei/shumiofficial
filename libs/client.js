cab150100ebc65edcd987ab2359a4464e9106ac0
shuhei_fujimura <shushu2525.1694@gmail.com>
2023-01-14 20:19:15 +0900

    clientを送信

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: 'shumiofficial1007',
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY || 'uqPEvqymW8AscBTlQ8aTIp5W6w8OfTNo1FgM',
 });
